6c1c62bc3f430c3602adc5ded5dc78d63985ad11
electric-head <h8h8red@gmail.com>
2018-05-05 16:03:38 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eh-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'eh';
+}
